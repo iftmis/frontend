6b00a15aed2291e87db7914144381d3d9edfd68d
Nicky Ngailo <nickyrabit@gmail.com>
2020-09-14 09:41:01 +0300

    commit dev

--- src/app/inspection-planning/inspection-activities/inspection-activities-detail/inspection-activities-detail.component.ts
+++ src/app/inspection-planning/inspection-activities/inspection-activities-detail/inspection-activities-detail.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 import { InspectionActivitiesService } from '../inspection-activities.service';
 import { InspectionActivitiesFormService } from './inspection-activities-form.service';
@@ -56,7 +56,7 @@ export class InspectionActivitiesDetailComponent implements OnInit {
   ) {
     // this.activityId = route.snapshot.parent?.parent?.params['id'];
   }
-  chaguliwaRisks = new Array();
+  // chaguliwaRisks = new Array();
 
   ngOnInit() {
     this.loadObjectives();
@@ -72,18 +72,33 @@ export class InspectionActivitiesDetailComponent implements OnInit {
   }
 
   loadRisks() {
+    // TODO get  by finacial year
     this.riskService.getAll().subscribe(response => {
-      this.risks.next(response);
+      //  this.risks.next(response);
+      this.allRisks = response;
+      console.log('all risk');
+      console.log(this.allRisks);
       this.loadAllSelectedRisks();
     });
   }
 
   removeAll(risks: Risk[]) {
-    this.riskService.removeAll(risks).subscribe(res => this.onSuccess());
+    // var pulled = _.pullAt(this.chosenRisks, [1, 3]);
+    // console.log( Object.keys(risks));
+    // this.chosenRisks.pop();
+    // this.riskService.removeAll(risks).subscribe(res => this.onSuccess());
+
+    const risksToAdd = risks.map(a => {
+      console.log(a.description + ' this is being removed');
+
+      const filteredRisks = this.chosenRisks.filter(item => item.id !== a.id);
+
+      console.log('Filtered list is');
+    });
   }
 
   getSelectedRisks(): Observable<Risk[]> {
-    return this.chosen.asObservable();
+    return of(this.chosenRisks);
   }
 
   saveAll(risk: Risk[]) {
@@ -95,8 +110,8 @@ export class InspectionActivitiesDetailComponent implements OnInit {
       console.log(a.description + ' this is being saved');
 
       // storing select risks in an array
-      this.chaguliwaRisks.push(a.id);
-      for (const i of this.chaguliwaRisks) {
+      this.chosenRisks.push(a);
+      for (const i of this.chosenRisks) {
         console.log('array has ' + i);
       }
 
@@ -114,17 +129,22 @@ export class InspectionActivitiesDetailComponent implements OnInit {
 
   loadAllSelectedRisks() {
     this.riskService.getByActivityId(this.activityId).subscribe(res => {
-      this.risks.next(res);
+      this.chosenRisks = res;
+      console.log('chosen risk');
+      console.log(this.chosenRisks);
       this.filterAreas(res);
     });
   }
 
   filterAreas(risk: any) {
-    const ids = risk.map((a: any) => a.riskId || 0);
+    const ids = risk.map((a: any) => a.id || 0);
+    console.log('ids', ids);
     // @ts-ignore
-    const filtered = this.allAreasAuditableAreas.filter((a: { id: any }) => {
+    const filtered = this.allRisks.filter((a: { id: any }) => {
       return ids.indexOf(a.id) === -1;
     });
+    console.log(filtered);
+
     this.risks.next(filtered);
   }
 
@@ -141,7 +161,7 @@ export class InspectionActivitiesDetailComponent implements OnInit {
   }
 
   getAllRisks(): Observable<Risk[]> {
-    return this.risks.asObservable();
+    //  return this.allRisks.asObservable();
   }
 
   loadAuditableAreas() {
